2e5ef279369450615d9d82402b3d263f7b09923e
ChrisG19 <christopherg2808@gmail.com>
2023-07-13 23:28:14 -0600

    Pruebas creación archivos

--- main.py
+++ main.py
@@ -8,12 +8,14 @@
 # Interfaz
 import tkinter as tk
 import subprocess
+import os
+
 from tkinter import ttk, Text, Label, Button
 
 def create_g4():
     # Texto completo
     texto = T.get("1.0", tk.END)
-    arch = "texto.g4"
+    arch = "Expr.g4"
     try:
         with open(arch, "w") as archivo:
             archivo.write(texto)
@@ -28,8 +30,23 @@ def create_g4():
         print("Error al ejecutar el comando:", e)
     
     # Crear archivo para imprimir arbol
+    archP = f"{arch[:-3]}Parser.py"
+    archL = f"{arch[:-3]}Lexer.py"
     
+    treeA = "Tree.py"
     
+    if os.path.exists(archP) and os.path.exists(archL):
+        with open(treeA, "w") as archi:
+            archi.write("from antlr4 import *\n")
+            archi.write(f"from {archP[:-3]} import {archP[:-3]}\n")
+            archi.write(f"from {archL[:-3]} import {archL[:-3]}\n\n")
+            archi.write("input_stream = InputStream(input('10+20*30'))\n")
+            archi.write(f"lexer = {archL[:-3]}(input_stream)\n")
+            archi.write("token_stream = CommonTokenStream(lexer)\n")
+            archi.write(f"parser = {archP[:-3]}(token_stream)\n")
+            archi.write("tree = parser.root()\n\n")
+            archi.write("print(tree.toStringTree(recog=parser))\n")
+
     # Texto por líneas
     # texto = T.get("1.0", "end-1c")  # Obtener el texto línea por línea
     # lineas = texto.split("\n")  # Dividir el texto en líneas separadas
