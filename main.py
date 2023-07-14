a57f44b0b456ce084544741a5c086d26cc6b730e
ChrisG19 <christopherg2808@gmail.com>
2023-07-13 23:00:30 -0600

    Commit inicial

--- main.py
+++ main.py
@@ -7,12 +7,28 @@
 
 # Interfaz
 import tkinter as tk
+import subprocess
 from tkinter import ttk, Text, Label, Button
 
-def obtener_texto():
+def create_g4():
     # Texto completo
     texto = T.get("1.0", tk.END)
-    print(texto)  
+    arch = "texto.g4"
+    try:
+        with open(arch, "w") as archivo:
+            archivo.write(texto)
+    except IOError as e:
+        print("Error al generar el archivo:", e)
+        
+    comando = ["antlr4", "-Dlanguage=Python3", str(arch)]
+
+    try:
+        subprocess.run(comando, check=True)
+    except subprocess.CalledProcessError as e:
+        print("Error al ejecutar el comando:", e)
+    
+    # Crear archivo para imprimir arbol
+    
     
     # Texto por líneas
     # texto = T.get("1.0", "end-1c")  # Obtener el texto línea por línea
@@ -35,7 +51,7 @@ space2 = Label(root, height=1)
 
 # Create an Exit button.
 b2 = Button(root, text="Exit", command=root.destroy)
-b3 = Button(root, text="Obtener Texto", command=obtener_texto)
+b3 = Button(root, text="Obtener Texto", command=create_g4)
 
 l.pack()
 T.pack()
