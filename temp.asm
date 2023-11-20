47f989168c4be77136616fc39371d8389b9f8759
Christopher García <60325784+ChristopherG19@users.noreply.github.com>
2023-11-20 00:37:01 -0600

    Cool version 3

--- temp.asm
+++ temp.asm
@@ -1,11 +1,12 @@
 .data
     newline: .asciiz "\n"
     str_0: .asciiz "Object"
-
+    str_1: .asciiz "Int"
+    str_2: .asciiz "5"    
 .text
 
 Main:
-    li $a0, 72
+    li $a0, 56
     li $v0, 9
     syscall
     move $s0, $v0
@@ -14,48 +15,61 @@ Main_main:
     sub $sp, $sp, 40
     sw $ra, 0($sp)
 
-    la $t0, str_0  # Cargar la dirección del literal de la cadena en el registro $t0
+    la $t0, str_0
     move $a2, $t0
-    li $t0, 3  # Índice de inicio del substring "Object"
-    li $t1, 3  # Longitud del substring
+    li $t0, 3
+    li $t1, 2
     move $a0, $t0
     move $a1, $t1
     jal String_substr
-    move $t0, $v0  # Posición inicial del substring
+    move $t0, $v0
     move $a0, $t0
-    jal IO_out_string  # Imprimir la posición inicial
+    jal IO_out_string
+
+    la $t0, str_1
+    move $a2, $t0
+    li $t0, 0
+    li $t1, 3
+    move $a0, $t0
+    move $a1, $t1
+    jal String_substr
+    move $t0, $v0
+    move $a0, $t0
+    jal IO_out_string
+    li $t4, 0
+
+    la $t0, str_2
+    move $a0, $t0
+    jal IO_out_string
+    li $t4, 0
 
     lw $ra, 0($sp)
     add $sp, $sp, 40
     j exit
 
 String_substr:
-    la $t2, str_0      # Puntero al inicio de la cadena
-    move $t0, $a0      # Índice de inicio del substring
-    move $t1, $a1      # Longitud del substring
-    add $t2, $t2, $t0  # Puntero a la posición inicial del substring
+    move $t2, $a2
+    move $t0, $a0
+    move $t1, $a1
+    add $t2, $t2, $t0
+    add $t4, $sp, $zero
+    add $t5, $t2, $t1
+loop:
+    bge $t2, $t5, end
+    lb $t3, ($t2)
+    sb $t3, ($t4)
+    addi $t2, $t2, 1
+    addi $t4, $t4, 1
+    j loop
 
-    add $t4, $sp, $zero  # Puntero al inicio del área reservada en la pila
-    add $t5, $t2, $t1    # Puntero al final del substring
-
-copy_loop:
-    bge $t2, $t5, end_substring  # Verificar si se llegó al final del substring
-
-    lb $t3, ($t2)      # Cargar el carácter del substring
-    sb $t3, ($t4)      # Almacenar el carácter en la pila
-    addi $t2, $t2, 1   # Mover al siguiente carácter del substring
-    addi $t4, $t4, 1   # Mover al siguiente espacio en la pila
-    j copy_loop
-
-end_substring:
-    move $v0, $sp      # Devolver el puntero al inicio del área copiada en la pila
+end:
+    move $v0, $sp
     jr $ra
 
 IO_out_string:
-    move $a0, $a0  # Cargar la dirección del substring
     li $v0, 4
     syscall
-    la $a0, newline  # Cargar el carácter de nueva línea
+    la $a0, newline
     li $v0, 4
     syscall
     jr $ra
